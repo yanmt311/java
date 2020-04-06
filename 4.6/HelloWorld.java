889aa87301a3facc52324868fba575da6e3be96b
Ymenging_MT <yanmt311@163.com>
2020-04-06 23:19:58 +0800

    java

---
+++ 4.6/HelloWorld.java
@@ -0,0 +1,43 @@
+/**
+ * 文档注释：
+ * 作者：GAOBO
+ * 日期：20200406
+ * 描述：
+ *      如果一个类是public的 那么这个类的类名  一定要和文件名相同，并且 一个java文件当中
+ * 只能有一个共有的类。
+ */
+
+//行注释
+/*
+块注释
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		/*
+		   1、整型int    4字节  
+		   2、标识符：数字  字母 下划线  $
+		 */
+		
+
+		int __________a = 0;
+
+		System.out.println(__________a);
+
+		// int max = Integer.MIN_VALUE-1;
+
+		// System.out.println(max);
+
+
+		// int a = 10;
+		// System.out.println(Integer.MAX_VALUE);
+		// System.out.println(Integer.MIN_VALUE);
+
+		// System.out.println(a);//打印 且换行
+		// System.out.print(a);//打印 但是不换行
+		// System.out.printf("%d\n",a);//以格式化的形式进行输出内容
+	}
+
+}
+
