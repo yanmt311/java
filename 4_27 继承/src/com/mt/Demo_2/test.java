66c860aee4f0a470f8a0d7f1dd53f22b84f1fe1e
Ymengting_MT <yanmt311@163.com>
2020-09-24 20:02:42 +0800

    java

--- 4_27 继承/src/com/mt/Demo_2/test.java
+++
@@ -1,9 +0,0 @@
-package com.mt.Demo_2;
-
-public class test {
-    public static void main(String[] args) {
-        //protected 同一包中不同类的访问
-        Animal animal = new Animal();
-        System.out.println(animal.name);
-    }
-}
--- 5_10 泛型/src/TestMain.java
+++
@@ -1,9 +0,0 @@
-public class TestMain {
-    public static void main(String[] args) {
-        MyArrayList myArrayList = new MyArrayList();
-        myArrayList.add(1);
-        myArrayList.add(2);
-        int val = myArrayList.getVal(1);
-        System.out.println(val);
-    }
-}
